e74a5da5ecb2bcffaa2d8f9e0c2d7a6b35849dec
ywsuoyi <donywang922@gmail.com>
2026-02-25 08:08:07 -0800

    A database suitable for user-collaborative filtering knn. -- Du Wang

---
+++ src/clean_db.py
@@ -0,0 +1,40 @@
+import sqlite3
+import os
+
+DB_NAME = "../data/user_first_clear.db"
+
+
+def clean_database():
+    initial_size = os.path.getsize(DB_NAME) / (1024 * 1024)
+    print(f"{initial_size:.2f} MB")
+
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+
+    cursor.execute("""
+                   DELETE
+                   FROM reviews
+                   WHERE movie_slug IN (SELECT slug
+                                        FROM movies
+                                        WHERE is_enriched = 0)
+                   """)
+    deleted_reviews = cursor.rowcount
+    print(f"delete {deleted_reviews} reviews")
+
+    cursor.execute("DELETE FROM movies WHERE is_enriched = 0")
+    deleted_movies = cursor.rowcount
+    print(f"delete {deleted_movies} movies")
+
+    conn.commit()
+
+    print("VACUUM")
+    cursor.execute("VACUUM")
+
+    conn.close()
+
+    final_size = os.path.getsize(DB_NAME) / (1024 * 1024)
+    print(f"{initial_size - final_size:.2f} MB -> {final_size:.2f} MB ")
+
+
+if __name__ == "__main__":
+    clean_database()
